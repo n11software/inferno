80154385b048e0d14d0dcf24cf5231412cd6225b
Ariston Lorenzo <me@ariston.dev>
2024-06-17 10:19:34 -0400

    Print out cpu id, family, model, and stepping
    I can set it up to return it in a separate function but I don't
    need it at the moment so I'll add it once I need it.

--- Source/CPU/CPUID.cpp
+++ Source/CPU/CPUID.cpp
@@ -12,31 +12,43 @@
 #include <Memory/Mem_.hpp>
 
 namespace CPU {
-    static char CPUModel[48];
+	static char CPUModel[48];
 
-    /* returns the CPU vendor ID */
-    const char *VendorID() {
-        unsigned long ebx, unused;
-        cpuid(0, unused, ebx, unused, unused);
-        switch (ebx) {
-            case 0x756e6547: /* Intel Magic Code */
-                return CPUID_VENDOR_INTEL;
-            case 0x68747541: /* AMD Magic Code */
-                return CPUID_VENDOR_AMD;
-            default:
-                return "Unknown";
-        }
-    }
+	/* returns the CPU vendor ID */
+	const char *VendorID() {
+		unsigned long ebx, unused;
+		cpuid(0, unused, ebx, unused, unused);	
+		switch (ebx) {
+			case 0x756e6547: /* Intel Magic Code */
+				return CPUID_VENDOR_INTEL;
+			case 0x68747541: /* AMD Magic Code */
+				return CPUID_VENDOR_AMD;
+			default:
+				return "Unknown";
+		}
+	}
 
-    /* returns the CPU model string  */
-    char *Model() {
-        unsigned int brand[12];
+	char *Model() {
+		int b, c, d, e;
+		int family, model, stepping;
+        	unsigned int id;
+		unsigned int brand[12];
 
-        cpuid(0x80000002, brand[0], brand[1], brand[2], brand[3]);
-        cpuid(0x80000003, brand[4], brand[5], brand[6], brand[7]);
-        cpuid(0x80000004, brand[8], brand[9], brand[10], brand[11]);
-        prDebug("cpu", "CPU0: %s", (char *)brand);
+		cpuid(0x80000002, brand[0], brand[1], brand[2], brand[3]);
+		cpuid(0x80000003, brand[4], brand[5], brand[6], brand[7]);
+		cpuid(0x80000004, brand[8], brand[9], brand[10], brand[11]);
+	
+		cpuid(1, b, c, d, e);
+	
+		id = b;
 
-        return (char *)brand;
-    }
+		stepping = b & 0xF;
+		model = (b >> 4) & 0xF;
+		family = (b >> 8) & 0xF;	        
+
+		prInfo("cpu", "CPU0: Origin=%s  Id=0x%x  Family=0x%x  Model=0x%x  Stepping=%d", VendorID(), id, family, model, stepping);
+		prInfo("cpu", "CPU0: %s", (char *)brand);
+
+		return (char *)brand;
+	}
 }
