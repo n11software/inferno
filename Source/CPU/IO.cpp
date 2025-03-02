796767d9989ba48b8f5b06a6fbc0837df8fb24f7
Levi Hicks <levih@n11.dev>
2025-03-01 20:32:45 -0600

    APIC Debug
    Signed-off-by: Levi Hicks <levih@n11.dev>

--- Source/CPU/IO.cpp
+++ Source/CPU/IO.cpp
@@ -7,6 +7,7 @@
 //===================================================================//
 
 #include <Inferno/IO.h>
+#include <Inferno/Log.h>
 
 void outb(unsigned short port, unsigned char value) {
 	asm volatile("outb %0, %1" :: "a"(value), "Nd"(port));
